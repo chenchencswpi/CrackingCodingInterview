49ed062b8fef4e40ec467fbdae0f62c6fa50317a
Chen <Chen@DESKTOP-QEC7584>
2016-08-21 10:28:54 -0400

    same

---
+++ src/arraysAndStrings/ZeroMatrix.java
@@ -0,0 +1,14 @@
+package arraysAndStrings;
+
+/**
+ * Write an algorithm such that if an element in an M*N matrix is 0, its entire row and column are set to 0.
+ * @author Chen
+ *
+ */
+public class ZeroMatrix {
+
+}
+
+class ZeroMatrixSolution {
+	
+}
