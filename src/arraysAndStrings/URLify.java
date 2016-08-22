88a21c934e51e41f811490a53d836b3b9b902da7
chenchen <chenchencswpi@gmail.com>
2016-08-21 22:55:54 -0400

    implement URLify method

--- src/arraysAndStrings/URLify.java
+++ src/arraysAndStrings/URLify.java
@@ -12,5 +12,50 @@ public class URLify {
 }
 
 class URLifySolution {
-	
+	/**
+	 * Ignore the fact that the array has sufficient space at the end to hold additional characters.
+	 * @param array
+	 * @param actualLength
+	 */
+	void replaceStringWithAdditionalDS(char[] array, int actualLength) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < actualLength; i++) {
+			if (array[i] != ' ') {
+				sb.append(array[i]);
+			} else {
+				sb.append("20%");
+			}
+		}
+		//put the string back to array;
+		String s = sb.toString();
+		for (int i = 0; i < s.length(); i++) {
+			array[i] = s.charAt(i);
+		}
+	}
+	/**
+	 * First determine how long the final length will be, and then start to convert.
+	 * @param array
+	 * @param actualLength
+	 */
+	void replaceStringWithoutAdditionalDS(char[] array, int actualLength) {
+		int realLength = 0;
+		for (int i = 0; i < actualLength; i++) {
+			if (array[i] == ' ') {
+				realLength += 3;
+			} else {
+				realLength += 1;
+			}
+		}
+		//start to convert from back
+		int cursor = realLength - 1;
+		for (int index = actualLength - 1; index >= 0; index--) {
+			if (array[index] != ' ') {
+				array[cursor--] = array[index]; 
+			} else {
+				array[cursor--] = '0';
+				array[cursor--] = '2';
+				array[cursor--] = '%';
+			}
+		}
+	}
 }
